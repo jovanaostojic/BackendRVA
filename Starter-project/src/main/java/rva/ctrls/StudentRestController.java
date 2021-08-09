fd3ccae4ff73f938b98dba74d372e3902af15efa
jovanaostojic <jojka.ostojic99@gmail.com>
2021-08-10 01:18:26 +0200

    Backend

--- Starter-project/src/main/java/rva/ctrls/StudentRestController.java
+++ Starter-project/src/main/java/rva/ctrls/StudentRestController.java
@@ -89,7 +89,7 @@ public class StudentRestController {
 		}
 		studentRepository.deleteById(id);
 		if(id==-100) {
-			jdbcTemplate.execute("INSERT INTO student VALUES (-100, 'Test', 'Student', 'TS1/1', 2, 8);");
+			jdbcTemplate.execute("INSERT INTO student VALUES (-100, 'Test', 'Student', 'TS1/1', -100, -100);");
 		}
 		return new ResponseEntity<Student>(HttpStatus.OK);
 	}
